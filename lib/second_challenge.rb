a9eb3e2a8607c1cbd35cf3d21d95963d23a29d00
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-18 04:33:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,15 +5,12 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends = []
-      } 
+      }, 
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }
-    
   }
-
-  
 end
