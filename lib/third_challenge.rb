cc93e4d8174efb2089da8841760eae8ac24ac3b8
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-18 04:35:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "54"
         },
       :matriarch => {
         #your key/value pairs here
